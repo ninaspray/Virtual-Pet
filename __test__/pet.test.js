d65147c25fff9c29b0ff7b22317ba7f628230c84
Nina Spray <ninaspray@gmail.com>
2021-01-16 15:57:16 +0000

    growUp

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -5,6 +5,7 @@ const pet = new Pet("Fido");
 
 //Return the pet as an object. 
 //test the pets name to be Fido. 
+//pet to have an initial age of 0.
 
 describe("Constructor", () => {
     it("Returns an object", () => {
@@ -12,6 +13,18 @@ describe("Constructor", () => {
     });
     it("Returns the pets name", () => {
         expect(pet.name).toBe("Fido");
+    });
+    it("Has an initial ag of 0", () => {
+        expect(pet.age).toBe(0);
+    });
+});
+
+//Create a fuction called growUp. So Pet grows by one year. 
+
+describe("growUp", () => {
+    it("icrements the pets age by 1", () => {
+        pet.growUp();
+        expect(pet.age).toEqual(1);
     });
 });
 
