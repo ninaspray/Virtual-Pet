b21097e6aa2d38ec449915657bc2887a1609e9f6
Nina Spray <ninaspray@gmail.com>
2021-01-16 16:07:25 +0000

    Method added to the objets prototype

--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,13 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.growUp = function () {
-        this.age += 1;
-    }
 };
 
+Pet.prototype.growUp = function () {
+    this.age++;
+};
 
+//adding a prototype function means each object of the same type should behave the same.
 
 
 
