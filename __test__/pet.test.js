926c63b3983edf9c58052ba75d9909635ced84a8
Nina Spray <ninaspray@gmail.com>
2021-01-16 16:57:25 +0000

    Age, Hunger, Fitness

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -19,13 +19,47 @@ describe("Constructor", () => {
     });
 });
 
+//Pet should have an initial hunger of 0. 
+
+describe("Hunger", () => {
+    it("pet should have an initial hunger of 0", () => {
+        expect(pet.hunger).toEqual(0);
+    });
+});
+
+//Pet should have an intial fitness of 10. 
+describe("Fitness", () => {
+    it("pet should have an initial fitness level of 10", () => {
+        expect(pet.fitness).toEqual(10);
+    });
+});
+
 //Create a fuction called growUp. So Pet grows by one year. 
 
 describe("growUp", () => {
     it("icrements the pets age by 1", () => {
+        pet.age = 0;
         pet.growUp();
         expect(pet.age).toEqual(1);
     });
 });
 
+//Grow up should increas hunger by 5 and decrease fitness by 3.
+describe("Pet becomes older and less fit", () => {
+    it("check the hunger", () => {
+        expect(pet.hunger).toBe(0);
+    });
+    it("Becomes hungrier", () => {
+        pet.growUp();
+        pet.becomesHungry();
 
+        expect(pet.hunger).toBe(5);
+    });
+    it("Becomes less fit", () => {
+        pet.growUp();
+        pet.becomesHungry();
+        pet.becomesUnfit();
+
+        expect(pet.fitness).toBe(7);
+    });
+})
--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,23 @@
-function Pet(name) {
+function Pet(name, age, hunger, fitness) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 };
 
-Pet.prototype.growUp = function () {
-    this.age++;
+Pet.prototype = {
+    growUp() {
+        this.age++;
+    },
+    becomesHungry() {
+        this.hunger += 5;
+    },
+    becomesUnfit() {
+        this.fitness -= 3;
+    }
 };
+
+
 
 //adding a prototype function means each object of the same type should behave the same.
 
