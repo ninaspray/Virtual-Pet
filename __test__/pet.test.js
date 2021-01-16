5781903aab1a270f6ad0184fafc6463093fe5d98
Nina Spray <ninaspray@gmail.com>
2021-01-16 15:36:05 +0000

    Changes

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,2 +1,18 @@
-const { pet } = require("../src/pet.js")
+const Pet = require("../src/pet");
+
+const pet = new Pet("Fido");
+
+
+//Return the pet as an object. 
+//test the pets name to be Fido. 
+
+describe("Constructor", () => {
+    it("Returns an object", () => {
+        expect(new Pet("Fido")).toBeInstanceOf(Object);
+    });
+    it("Returns the pets name", () => {
+        expect(pet.name).toBe("Fido");
+    });
+});
+
 
